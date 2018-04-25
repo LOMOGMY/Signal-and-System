6282011b486f264d080cc20aef6445988562734c
G_MY <30328316+LOMOGMY@users.noreply.github.com>
2018-04-25 20:09:04 +0800

    Update LAB2_3_b.m

--- NO.4/LAB2_3_b.m
+++ NO.4/LAB2_3_b.m
@@ -1,13 +1,17 @@
+syms T;
 t=[0:0.5:10]
 b=[1]
 a=[1 0.5]
 s=step(b,a,t) 
-subplot(2,1,1)
+subplot(3,1,1)
 plot(t,s,'--','color',[0.1 0.5 1])
 xlabel('t')
 ylabel('step')
 h=impulse(b,a,t)
-subplot(2,1,2)
+subplot(3,1,2)
 plot(t,h,'--','color',[1 0.1 0.5])
 xlabel('t')
 ylabel('Impulse')
+subplot(3,1,3)
+s_ = 2*(1-exp(-T/2));
+ezplot(diff(s_),[0,10,0,1])
